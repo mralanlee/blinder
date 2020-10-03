d0a45e7c3c88b9f1ff3d9aaface1e531a96bd43f
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-10-03 18:12:07 -0300

    Change user model to add phone field

--- api/src/blinder/settings.py
+++ api/src/blinder/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# Substituting a custom User model
+# https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#substituting-a-custom-user-model
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
